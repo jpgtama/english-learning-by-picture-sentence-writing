a26d2f804ed041275737651dc5e3f29b0038eb2b
Hu, Xiaoyi <evan.xy.hu@philips.com>
2017-04-25 10:24:12 +0800

    add real parse ability.

--- js/widgets.js
+++ js/widgets.js
@@ -1,108 +1,150 @@
-class EditorTextAreaWidget extends BaseWidget{
-    constructor(parentNode, /* String('readonly', 'editable') */mode = 'readonly', contentStr){
-        super(parentNode, `<div class="EditorTextAreaWidget">
-            <div class="readOnlyNode">
-                <div class="toolbar"></div>
-                <div class="content"></div>
-            </div>
-            <div class="editableNode">
-                <div class="toolbar"></div>
-                <textarea class="content" cols="100" rows="2"></textarea>
-            </div>
-        </div>`);
 
-        this.readOnlyNode = this.domNode.querySelector('.readOnlyNode');
-        this.editableNode = this.domNode.querySelector('.editableNode');
-
-        // add tool bar widget
-        new ToolBarWidget(this.readOnlyNode.querySelector('.toolbar'), {Edit: ()=>{
-            this.editableMode();
-        }});
-
-        new ToolBarWidget(this.editableNode.querySelector('.toolbar'), {Done: ()=>{
-            this.readonlyMode();
-        }});
-
-        // set content
-        if(contentStr){
-            this.readOnlyValue = contentStr;
-            this.editableValue = contentStr;
-        }
-
-        // select mode
-        if(mode === 'readonly'){
-            this.readonlyMode();
-        }else if(mode === 'editable'){
-            this.editableMode();
-        }else{
-            throw 'not supported mode';
-        }
+class ChineseEnglishSentenceWidget{
+    constructor(rootDom){
+        this.rootDom = rootDom;
+        this.enableRemoveOperation();
+        this.enableAddOperation();
     }
 
-    set readOnlyValue(v){
-        this.readOnlyNode.querySelector('.content').innerText = v;
-    }
-    get readOnlyValue(){
-        return this.readOnlyNode.querySelector('.content').innerText;
-    }
 
-    set editableValue(v){
-        this.editableNode.querySelector('textarea').value = v;
-    }
 
-    get editableValue(){
-        return this.editableNode.querySelector('textarea').value;
-    }
+    getContentRowTemplate(chinese, english){
+        var templateStr =  `    <tr>
+                <th scope="row" class="index-number"></th>
+                <td>
+                    <div>
+                        <div class="chinese">
+                            <textarea >${chinese}</textarea>
+                        </div>
+                        <div class="english">
+                            <textarea >${english}</textarea>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <span class="operation-remove">Remove</span>
+                </td>
+            </tr>`;
 
-    readonlyMode(){
-        this.readOnlyValue = this.editableValue;
-
-        this.setEditableNodeDisplay(false);
-        this.setReadOnlyNodeDisplay(true);
-    }
-
-    editableMode(){
-        this.setReadOnlyNodeDisplay(false);
-        this.setEditableNodeDisplay(true);
-    }
-
-    setEditableNodeDisplay(isShow){
-        this.editableNode.style.display = isShow? 'table' : 'none';
+        var div = document.createElement('table');
+        div.innerHTML = templateStr;
+        var result = div.querySelector('tr');
+        result.parentElement.removeChild(result);
+        return result;
     }
 
-    setReadOnlyNodeDisplay(isShow){
-        this.readOnlyNode.style.display = isShow? 'table' : 'none';
-    }
-}
-
-class ToolBarWidget extends BaseWidget{
-    constructor(parentNode, buttons = {Done: ()=> {alert('Default Toolbar Item')}}){
-
-        //  parse buttons
-        var itemsHtml = '';
-
-        Object.keys(buttons).forEach(k => {
-            var span = `<span class="item">${k}</span>`;
-            itemsHtml += span;
-        });
-
-        var template = `<div class="toolbarWidget">${itemsHtml}</div>`;
-
-        super(parentNode, template);
-
-        this.buttons = buttons;
-        this.setListener();
+    get contentBody(){
+        return this.rootDom.querySelector('tbody');
     }
 
-    setListener(){
-        this.domNode.addEventListener('click', e => {
-            var innerText = e.target.innerText;
+    get contentTRs(){
+        return this.rootDom.querySelectorAll('tbody tr');
+    }
 
-            if(this.buttons[innerText]){
-                this.buttons[innerText]();
+    get indexNumberDoms(){
+        return this.rootDom.querySelectorAll('tbody .index-number');
+    }
+
+    getAncestor(dom, isAncestor){
+        var check = p => {
+
+            if(!p){
+                return undefined;
             }
 
+            if(isAncestor(p)){
+                return p;
+            }else{
+                return check(p.parentElement);
+            }
+        };
+
+        return check(dom.parentElement);
+    }
+
+    enableAddOperation(){
+        this.rootDom.querySelector('.operation-add').addEventListener('click', e =>{
+            var rowTemplate = this.getContentRowTemplate('', '');
+            this.contentBody.appendChild(rowTemplate);
+            this.updateIndexNumber();
         });
     }
 
+    enableRemoveOperation(){
+        this.rootDom.addEventListener('click', e => {
+            if(e.target.classList.contains('operation-remove')){
+                var confirm = window.confirm('Are you sure?');
+                if(confirm){
+                    // remove
+                    this.operation_remove(this.getAncestor(e.target, p => { return p.tagName === 'TR'; }));
+                }
+            }
+        });
+    }
+
+    operation_remove(tr){
+        tr.parentElement.removeChild(tr);
+        this.updateIndexNumber();
+    }
+
+    updateIndexNumber(){
+        var indexNumber = 1;
+        this.indexNumberDoms.forEach(dom => {
+            dom.innerText = indexNumber++;
+        });
+    }
+
+    static get rootDomTemplate(){
+        var div = document.createElement('div');
+        var templateStr = `<table class="table table-striped chinese-english-sentence-widget">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>句子</th>
+                    <th>操作</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="3" class="operation-add">
+                            Add
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>`;
+
+        div.innerHTML = templateStr;
+        var table = div.querySelector('table');
+        table.parentElement.removeChild(table);
+        return table;
+    }
+
+    static parse(){
+        // find out all element with 'data-widget=chinese-english-sentence-widget' and replace it with template
+        document.querySelectorAll('[data-widget=chinese-english-sentence-widget]').forEach(dom => {
+            // replace
+            var templateDom = ChineseEnglishSentenceWidget.rootDomTemplate;
+            dom.parentElement.insertBefore(templateDom, dom);
+            dom.parentElement.removeChild(dom);
+        });
+
+        // parse, create widget instance and associate with root dom
+        document.querySelectorAll('.chinese-english-sentence-widget').forEach(dom => {
+            var widget = new ChineseEnglishSentenceWidget(dom);
+            if(!ChineseEnglishSentenceWidget.widgetList){
+                ChineseEnglishSentenceWidget.widgetList = [];
+            }
+
+            ChineseEnglishSentenceWidget.widgetList.push(widget);
+        });
+    }
+
+    static list(){
+        return ChineseEnglishSentenceWidget.widgetList;
+    }
 }
+
+
+ChineseEnglishSentenceWidget.parse();
