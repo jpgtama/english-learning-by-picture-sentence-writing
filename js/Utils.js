ae9744ffccf33b12f18444a3e309cff9aa7559a1
Evan Hu <jpgtama@gmail.com>
2017-04-30 09:17:39 +0800

    adopt require.js

--- js/Utils.js
+++ js/Utils.js
@@ -12,3 +12,55 @@ var MyMockHTTPRequest = function(){
 
 
 };
+
+
+var URLRouter = function(){
+  this.currentUrl = window.location.href;
+  this.currentPath = window.location.pathname;
+  
+  
+  // relative
+    var relativePath = function(currentPath, newPath){
+      var currentSteps = currentPath.split('/');
+      var pathSteps = newPath.split('/');
+      
+      while(pathSteps.length> 0){
+        var step = pathSteps.shift();
+        if(step === '..'){
+          // go up
+          if(currentSteps.length<=1){
+            // check 
+            throw 'no more up path';
+          }
+          
+          currentSteps.pop();
+          
+        }else if(step === '.'){
+          // stay
+          
+        }else{
+          // go down 
+          currentSteps.push(step);
+        }
+      
+      };
+      
+      var finalPath = currentSteps.join('/');
+      return finalPath;
+    };
+  
+        // absolute
+    var absolutePath = function(path){
+      return path;
+    };
+  
+  this.goPath = function(path){
+    
+    if(path.startsWith('/')){
+      location.pathname = absolutePath(path);
+    }else{
+      location.pathname = relativePath(path);
+    }
+   
+  };
+};
